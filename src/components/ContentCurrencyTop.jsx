0c292a898a14276d8386c3018c759304e7197826
cyber-wf-13 <o.zon77788@gmail.com>
2022-09-22 06:35:15 +0300

    Add connect with localStorage for ConvertPortable and table

--- src/components/ContentCurrencyTop.jsx
+++ src/components/ContentCurrencyTop.jsx
@@ -11,15 +11,59 @@ const ContentCurrencyTop = () => {
     "1 hour",
     "24 hour",
     "USD",
+    "Compare",
+    "Delete",
   ];
 
   const [tableContent, setTableContent] = useState([]);
+  const [comparedCurrency, setComparedCurrency] = useState([]);
 
   useEffect(() => {
     convert.getTableData().then((tableData) => {
       setTableContent(tableData);
     });
+
+    const fromLocalStorage = [];
+
+    if (localStorage.getItem("convertFrom")) {
+      fromLocalStorage.push(localStorage.getItem("convertFrom"));
+    }
+
+    if (localStorage.getItem("convertTo")) {
+      fromLocalStorage.push(localStorage.getItem("convertTo"));
+    }
+
+    setComparedCurrency([...comparedCurrency, ...fromLocalStorage]);
   }, []);
+
+  const clickCompareBtn = function (currencyId) {
+    if (comparedCurrency.length === 2) {
+      console.log("many for compare");
+      return;
+    }
+    setComparedCurrency([currencyId, ...comparedCurrency]);
+
+    if (comparedCurrency.length === 0) {
+      localStorage.setItem("convertFrom", currencyId);
+    }
+
+    if (comparedCurrency.length === 1) {
+      localStorage.setItem("convertTo", currencyId);
+    }
+  };
+
+  const clickResetCompareBtn = function (currencyId) {
+    const idCurrencyFromArray = comparedCurrency.indexOf(currencyId);
+    const comparedArray = [...comparedCurrency];
+    comparedArray.splice(idCurrencyFromArray, 1);
+    setComparedCurrency(comparedArray);
+
+    if (localStorage.getItem("convertFrom") === currencyId) {
+      localStorage.removeItem("convertFrom");
+    } else if (localStorage.getItem("convertTo") === currencyId) {
+      localStorage.removeItem("convertTo");
+    }
+  };
 
   return (
     <div className="content-currency">
@@ -53,6 +97,30 @@ const ContentCurrencyTop = () => {
                 {content.change24Hour.value}
               </td>
               <td className="calc-table__body-ceil">{content.usdPrice}</td>
+              <td className="calc-table__body-ceil">
+                <button
+                  className="calc-table__button"
+                  onClick={() => {
+                    clickCompareBtn(content.name);
+                  }}
+                >
+                  Add to compare
+                </button>
+              </td>
+              {comparedCurrency.includes(content.name) ? (
+                <td className="calc-table__body-ceil">
+                  <button
+                    className="calc-table__button calc-table__button--reset"
+                    onClick={() => {
+                      clickResetCompareBtn(content.name);
+                    }}
+                  >
+                    Remove from compare
+                  </button>
+                </td>
+              ) : (
+                ""
+              )}
             </tr>
           );
         })}
